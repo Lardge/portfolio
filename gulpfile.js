f4f0956556faef65f4b24020c5cce54c962fc5df
Lardge <larsbf@gmail.com>
2016-11-13 10:30:14 +0100

    gulp and stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function () {
-    // place code for your default task here
+    return gulp.src('./css/style.css')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/'));
 });
