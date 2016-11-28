a093f051f3ee99fcfee2d12f51bedc18c1352dfc
Lardge <larsbf@gmail.com>
2016-11-28 19:15:22 +0100

    fix for larsbf.se

--- angular/directives.js
+++ angular/directives.js
@@ -4,55 +4,55 @@ app.directive('header', function () {
     return {
         priority: 1,
         require: '^^loader',
-        templateUrl: '../html/1-header.html'
+        templateUrl: 'html/1-header.html'
     };
 });
 app.directive('home', function () {
     return {
         priority: 2,
         require: '^^loader',
-        templateUrl: '../html/2-home.html'
+        templateUrl: 'html/2-home.html'
     };
 });
 app.directive('about', function () {
     return {
         priority: 3,
         require: '^^loader',
-        templateUrl: '../html/3-about.html'
+        templateUrl: 'html/3-about.html'
     };
 });
 app.directive('timeline', function () {
     return {
         priority: 4,
         require: '^^loader',
-        templateUrl: '../html/4-timeline.html'
+        templateUrl: 'html/4-timeline.html'
     };
 });
 app.directive('fun', function () {
     return {
         priority: 5,
         require: '^^loader',
-        templateUrl: '../html/5-fun.html'
+        templateUrl: 'html/5-fun.html'
     };
 });
 app.directive('contact', function () {
     return {
         priority: 6,
         require: '^^loader',
-        templateUrl: '../html/6-contact.html'
+        templateUrl: 'html/6-contact.html'
     };
 });
 app.directive('ngfooter', function () {
     return {
         priority: 7,
         require: '^^loader',
-        templateUrl: '../html/7-footer.html'
+        templateUrl: 'html/7-footer.html'
     };
 });
 app.directive('loader', function () {
     return {
         priority: 0,
-        templateUrl: '../html/8-loader.html'
+        templateUrl: 'html/8-loader.html'
     };
 });
 //INITIALIZE MATERILIZE FUNCTIONS
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
 gulp.task('default', function () {
-    return gulp.src('./css/style.css')
+    return gulp.src('css/style.css')
         .pipe(uglify())
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest('dist/'));
 });
